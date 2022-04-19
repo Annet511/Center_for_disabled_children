a3d7c198148e09368c29c141b5ad2ab2e8db26f6
nnnnnns <natkaa46@gmail.com>
2022-04-19 14:02:22 +0400

    add

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.certificates-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 500,
+    dots: true,
+    adaptiveHeight: true,
+    lazyLoad: 'ondemand',
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+    draggable:false,
+    // rows:2,
+    
+  });
+});
