4d51536753bf767cf35e5d1b98b600d7308ef300
Sweetleo30 <diana790@inbox.ru>
2022-04-27 20:49:48 +0300

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-$(document).ready(function(){
-  $('.certificates-slider').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    speed: 500,
-    dots: true,
-    adaptiveHeight: true,
-    lazyLoad: 'ondemand',
-    // autoplay: true,
-    // autoplaySpeed: 1000,
-    draggable:false,
-    // rows:2,
-  });
-});
 $(function () {
     var $videoContainer = $('#video'),
         $videoControls = $('.video-control'),
@@ -68,14 +53,13 @@ function closeOnClick() {
 
 // Плавный переход к якорю
 
-if(window.location.toString().indexOf('index.html')>0)
-{
+if (window.location.toString().indexOf('index.html') > 0) {
     const anchors = document.querySelectorAll('a[href*="#"]')
     for (let anchor of anchors) {
-        anchor.addEventListener("click", function(event) {
+        anchor.addEventListener("click", function (event) {
             event.preventDefault();
             const blockID = anchor.getAttribute('href')
-            document.querySelector(''+ blockID).scrollIntoView({
+            document.querySelector('' + blockID).scrollIntoView({
                 behavior: "smooth",
                 block: "start"
             })
